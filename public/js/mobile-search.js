b43ea6c99d510292407cb9ec14a6d5bbd41e1fb1
Ogstra <ogstragal@gmail.com>
2023-07-30 02:27:36 -0300

    cambios en NavBar
    +boton search en navbar \ Logo: -display:flex (estaba corrido hacia arriba) \ Nav Icons: +display:flex (en mobile se veian mal) \ Nav: position:absolute important (por problemas visuales...)

---
+++ public/js/mobile-search.js
@@ -0,0 +1,8 @@
+function openSearch() {
+    document.getElementById("myNavSearch").style.display = "flex";
+
+}
+
+function closeSearch() {
+    document.getElementById("myNavSearch").style.display = "none";
+}
